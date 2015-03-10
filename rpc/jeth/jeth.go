8b0796b3a1e2c9f77ce6633f9e6fa1d8f826848e
zelig <viktor.tron@gmail.com>
2015-03-11 04:33:09 +0700

    fix provider

--- rpc/jeth/jeth.go
+++ rpc/jeth/jeth.go
@@ -2,9 +2,9 @@ package jeth
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/obscuren/otto"
 
-	"github.com/ethereum/go-ethereum/logger"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
@@ -12,8 +12,6 @@ const (
 	jsonrpcver       = "2.0"
 	maxSizeReqLength = 1024 * 1024 // 1MB
 )
-
-var jlogger = logger.NewLogger("RPC-JEth")
 
 type Jeth struct {
 	ethApi *rpc.EthereumApi
@@ -25,33 +23,32 @@ func New(ethApi *rpc.EthereumApi, toVal func(interface{}) otto.Value) *Jeth {
 }
 
 func (self *Jeth) err(code int, msg string, id interface{}) otto.Value {
-	jsonerr := &rpc.RpcErrorObject{code, msg}
-	response := &rpc.RpcErrorResponse{JsonRpc: jsonrpcver, ID: id, Error: jsonerr}
-	result, _ := json.Marshal(response)
-	return self.toVal(result)
+	rpcerr := &rpc.RpcErrorObject{code, msg}
+	rpcresponse := &rpc.RpcErrorResponse{JsonRpc: jsonrpcver, ID: id, Error: rpcerr}
+	return self.toVal(rpcresponse)
 }
 
-func (self *Jeth) Send(call otto.FunctionCall) (ottoResponse otto.Value) {
-	jlogger.DebugDetailln("Handling request")
-	jsonreq, err := call.Argument(0).ToString()
+func (self *Jeth) Send(call otto.FunctionCall) (response otto.Value) {
+	reqif, err := call.Argument(0).Export()
 	if err != nil {
 		return self.err(-32700, err.Error(), nil)
 	}
-	if len(jsonreq) > maxSizeReqLength {
-		return self.err(-32700, "Error: Request too large", nil)
-	}
-	var req rpc.RpcRequest
-	err = json.Unmarshal([]byte(jsonreq), &req)
-	if err != nil {
-		return self.err(-32700, err.Error(), nil)
-	}
+	fmt.Printf("reqif: %#v", reqif)
 
-	var response interface{}
-	err = self.ethApi.GetRequestReply(&req, &response)
+	jsonreq, err := json.Marshal(reqif)
+
+	var req rpc.RpcRequest
+	err = json.Unmarshal(jsonreq, &req)
+	fmt.Printf("req: %#v", req)
+
+	var respif interface{}
+	err = self.ethApi.GetRequestReply(&req, &respif)
 	if err != nil {
 		return self.err(-32603, err.Error(), req.ID)
 	}
-	rpcresponse := &rpc.RpcSuccessResponse{JsonRpc: jsonrpcver, ID: req.ID, Result: response}
-	result, _ := json.Marshal(rpcresponse)
-	return self.toVal(result)
+	rpcresponse := &rpc.RpcSuccessResponse{JsonRpc: jsonrpcver, ID: req.ID, Result: respif}
+	fmt.Printf("rpcresponse: %#v", rpcresponse)
+	response = self.toVal(rpcresponse)
+	fmt.Printf("response: %#v", response)
+	return
 }
